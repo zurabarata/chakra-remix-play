42ee214cbff2265a6773e6162a07b3247f596a37
Zurab <zurab@candis.io>
2022-04-17 02:17:22 +0200

    add a random notes

--- app/routes/me/NoteCard.tsx
+++ app/routes/me/NoteCard.tsx
@@ -48,7 +48,7 @@ export default function NoteCard() {
                     mx={-6}
                     mb={6}
                     pos={'relative'}>
-{/*                    <Image
+{/*                    <img
                         src={
                             'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
                         }
